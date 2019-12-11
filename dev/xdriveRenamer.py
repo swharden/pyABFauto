09eb9d740ae6c35064511d190d07e9474218719d
Scott W Harden <swharden@gmail.com>
2019-12-11 15:12:24 -0500

    Create xdriveRenamer.py

---
+++ dev/xdriveRenamer.py
@@ -0,0 +1,23 @@
+"""
+This script finds all autoanalysis folders named "swhlab" and renames them to "_autoanalysis"
+"""
+
+import os
+
+logFile = os.path.join(os.path.dirname(__file__), "oldPaths.txt")
+
+
+def locateAutoanalysisFolders(rootPath, oldName="swhlab"):
+    dirCount = 0
+    os.remove(logFile)
+    with open(logFile, 'a') as f:
+        for dirPath, subdirList, fileList in os.walk(rootPath):
+            if os.path.basename(dirPath).lower() == oldName.lower():
+                dirCount += 1
+                print(f"{dirCount}: {dirPath}")
+                f.write(dirPath+"\n")
+    print(f"wrote: {logFile}")
+
+
+if __name__ == "__main__":
+    locateAutoanalysisFolders(r"X:\Data")
