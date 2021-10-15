2c22cc13793d97803dfb989139b98bdb48ee5506
Scott W Harden <swharden@gmail.com>
2021-10-15 15:28:58 -0400

    0913: add memtest information

--- src/pyABFauto/analyses/jeff.py
+++ src/pyABFauto/analyses/jeff.py
@@ -2,6 +2,7 @@
 import pyABFauto
 from typing import Tuple
 import pyabf
+import pyabf.tools.memtest
 import numpy as np
 import scipy.optimize
 import matplotlib.pyplot as plt
@@ -20,6 +21,12 @@ def getMean(abf: pyabf.ABF, start: float, end: float) -> float:
     i1 = int(start * abf.sampleRate)
     i2 = int(end * abf.sampleRate)
     return np.mean(abf.sweepY[i1:i2])
+
+
+def getMin(abf: pyabf.ABF, start: float, end: float) -> float:
+    i1 = int(start * abf.sampleRate)
+    i2 = int(end * abf.sampleRate)
+    return np.min(abf.sweepY[i1:i2])
 
 
 def getCurveIndexes(sweep: np.ndarray, antipeakIndex: int, baselineMean: float,
@@ -126,6 +133,27 @@ def plotAreaBySweep(abf: pyabf.ABF, ax: matplotlib.axes.Axes, epoch: int = 3):
     ax.set_xlabel("Time (minutes)")
     ax.grid(alpha=.5, ls='--')
     addTagLines(abf, ax)
+    
+
+def plotPeakBySweep(abf: pyabf.ABF, ax: matplotlib.axes.Axes, epoch: int = 3):
+
+    puffTimeStart = abf.sweepEpochs.p1s[epoch] / abf.sampleRate
+    puffTimeEnd = abf.sweepEpochs.p2s[epoch] / abf.sampleRate
+
+    values = []
+    for i in range(abf.sweepCount):
+        abf.setSweep(i)
+        baselineStart = puffTimeStart - .1
+        baselineEnd = puffTimeStart
+        baselineMean = getMean(abf, baselineStart, baselineEnd)
+        antipeak = getMin(abf, puffTimeEnd, puffTimeEnd+.5) - baselineMean
+        values.append(abs(antipeak - baselineMean))
+
+    ax.plot(abf.sweepTimesMin, values, '.-', color='k')
+    ax.set_ylabel("Peak Response (pA)")
+    ax.set_xlabel("Time (minutes)")
+    ax.grid(alpha=.5, ls='--')
+    addTagLines(abf, ax)
 
 
 def plotTimeAfterBySweep(abf: pyabf.ABF, ax: matplotlib.axes.Axes, epoch: int = 3):
@@ -150,15 +178,46 @@ def plotTimeAfterBySweep(abf: pyabf.ABF, ax: matplotlib.axes.Axes, epoch: int = 
     ax.grid(alpha=.5, ls='--')
     addTagLines(abf, ax)
 
-
 def addTagLines(abf: pyabf.ABF, ax: matplotlib.axes.Axes):
     for tagTime in abf.tagTimesMin:
         ax.axvline(tagTime, linewidth=2, color='r', alpha=.5, linestyle='--')
 
+def plotMemtestResults(abf: pyabf.ABF, ax1: matplotlib.axes.Axes, ax2: matplotlib.axes.Axes, ax3: matplotlib.axes.Axes):
+    mt = pyabf.tools.memtest.Memtest(abf)
+    
+    ax1.grid(alpha=.5, ls='--')
+    ax1.set_ylabel("Ih (pA)")
+    ax1.set_xlabel("Time (minutes)")
+    ax1.plot(abf.sweepTimesMin, mt.Ih.values, '.-', color='b')
+    addTagLines(abf, ax1)
+    
+    ax2.grid(alpha=.5, ls='--')
+    ax2.set_ylabel("Rm (MΩ)")
+    ax2.set_xlabel("Time (minutes)")
+    ax2.plot(abf.sweepTimesMin, mt.Rm.values, '.-', color='r')
+    addTagLines(abf, ax2)
+    
+    ax3.grid(alpha=.5, ls='--')
+    ax3.set_ylabel("Ra (MΩ)")
+    ax3.set_xlabel("Time (minutes)")
+    ax3.plot(abf.sweepTimesMin, mt.Ra.values, '.-', color='k')
+    addTagLines(abf, ax3)
+
+def plotFullAbf(abf: pyabf.ABF, ax: matplotlib.axes.Axes):
+    for sweep in range(abf.sweepCount):
+        abf.setSweep(sweep, absoluteTime=True)
+        ax.plot(abf.sweepX / 60, abf.sweepY, 'b-')
+    ax.margins(0, .1)
+    ax.set_ylabel("Current (pA)")
+    ax.set_xlabel("Time (minutes)")
+    addTagLines(abf, ax)
 
 def tau(abf: pyabf.ABF, fig: pyABFauto.figure.Figure):
-    fig, axs = plt.subplots(2, 2, figsize=(8, 6))
+    fig, axs = plt.subplots(3, 3, figsize=(12, 12))
     measureTau(abf, 0, ax=axs[0, 0])
     plotTauBySweep(abf, axs[0, 1])
     plotAreaBySweep(abf, axs[1, 0])
     plotTimeAfterBySweep(abf, axs[1, 1])
+    plotPeakBySweep(abf, axs[0, 2])
+    plotMemtestResults(abf, axs[2, 0], axs[2, 1], axs[2, 2])
+    plotFullAbf(abf, axs[1, 2])
