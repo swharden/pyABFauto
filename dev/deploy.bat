dae6fe706c9bb2f4ef1eecc31e9a0459dfc29ba0
Scott W Harden <swharden@gmail.com>
2022-01-31 10:57:57 -0500

    update deployment script

--- dev/deploy.bat
+++ dev/deploy.bat
@@ -2,7 +2,7 @@
 :: not have internet access and can't pull the latest code using git
 
 rmdir X:\Software\pyABFauto /Q /S
-robocopy ..\src\pyABFauto X:\Software\pyABFauto /E /NJH /NFL /NDL
-rmdir X:\Software\pyABFauto\__pycache__ /Q /S
-rmdir X:\Software\pyABFauto\analyses\__pycache__ /Q /S
+robocopy ..\..\pyABFauto X:\Software\pyABFauto /E /NJH /NFL /NDL
+rmdir X:\Software\pyABFauto\src\pyABFauto\__pycache__ /Q /S
+rmdir X:\Software\pyABFauto\src\pyABFauto\analyses\__pycache__ /Q /S
 pause
