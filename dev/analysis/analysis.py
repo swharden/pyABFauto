2d43271400762cfb67c8ea2ee21bdc416ec85bbf
Scott W Harden <swharden@gmail.com>
2023-05-11 11:21:29 -0400

    improve gain analysis

--- dev/analysis/analysis.py
+++ dev/analysis/analysis.py
@@ -1,5 +1,6 @@
 import pathlib
 import pyabf
+import pyabf.tools.ap
 import pyabf.filter
 import numpy as np
 import json
@@ -28,7 +29,41 @@ def analyze_rmp(abf_path: pathlib.Path):
     _save_json(abf_path, values, "rmp", 1)
 
 
+def analyze_gain(abf_path: pathlib.Path, epoch: int):
+    abf = pyabf.ABF(abf_path)
+
+    indexMin = abf.sweepEpochs.p1s[epoch]
+    indexMax = abf.sweepEpochs.p2s[epoch]
+    indexMid = int((indexMax + indexMin) / 2)
+
+    sec1 = indexMin / abf.dataRate
+    sec2 = indexMax / abf.dataRate
+    print(f"Analyzing APs between {sec1} and {sec2} sec")
+
+    counts = [None] * abf.sweepCount
+    currents = [None] * abf.sweepCount
+
+    for sweep in range(abf.sweepCount):
+        abf.setSweep(sweep)
+        indexes = pyabf.tools.ap.ap_points_currentSweep(abf)
+        indexes = [x for x in indexes if x >= indexMin and x <= indexMax]
+        counts[sweep] = len(indexes)
+        currents[sweep] = abf.sweepC[indexMid]
+
+    values = {"counts": counts, "currents": currents}
+    _save_json(abf_path, values, "gain", 1)
+
+
+def analyze_all_protocol(folder: pathlib.Path, protocol: str):
+    for abf_path in folder.glob("*.abf"):
+        print(abf_path)
+        abf = pyabf.ABF(abf_path, loadData=False)
+        if not protocol in abf.protocol:
+            continue
+        print("ANALYZING")
+        analyze_gain(abf_path, 2)
+
+
 if __name__ == "__main__":
-    abf_path = "X:/Data/zProjects/NTS CSDS/abfs/2023-05-05/2023_05_05_0044.abf"
-    abf_path = pathlib.Path(abf_path)
-    analyze_rmp(abf_path)
+    path = "X:/Data/zProjects/NTS CSDS/abfs/2023-05-10"
+    analyze_all_protocol(pathlib.Path(path), "0143")
