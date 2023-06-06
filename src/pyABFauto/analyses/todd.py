03c32671b45f117a1a1ac8c83285aada91a5af2c
Scott W Harden <swharden@gmail.com>
2023-06-06 14:29:45 -0400

    protocol: 0910 (delta from step)

--- src/pyABFauto/analyses/todd.py
+++ src/pyABFauto/analyses/todd.py
@@ -10,7 +10,7 @@ import numpy as np
 
 def vc_ican(abf: pyabf.ABF, fig: pyABFauto.figure.Figure):
 
-    pyabf.filter.gaussian(abf, 10)
+    pyabf.filter.gaussian(abf, 20)
 
     cmap = plt.cm.get_cmap('rainbow')
     for sweepIndex in range(abf.sweepCount):
@@ -18,7 +18,7 @@ def vc_ican(abf: pyabf.ABF, fig: pyABFauto.figure.Figure):
         abf.setSweep(sweepIndex)
         plt.plot(abf.sweepX, abf.sweepY, color=cmap(fraction), label=f"sweep {sweepIndex+1}")
 
-    plt.legend(loc="upper right")
+    #plt.legend(loc="upper right")
     plt.ylabel(abf.sweepLabelY)
     plt.xlabel(abf.sweepLabelX)
     plt.grid(alpha=.5, ls='--')
