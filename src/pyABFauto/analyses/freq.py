3d6fa3a74b20fdc4bec837105c94ab604a82a1c1
Scott W Harden <swharden@gmail.com>
2023-08-15 14:50:16 -0400

    protocol: breathing rate

--- src/pyABFauto/analyses/freq.py
+++ src/pyABFauto/analyses/freq.py
@@ -5,6 +5,8 @@ import numpy as np
 import pyabf
 import pathlib
 import pyabf
+import scipy
+import scipy.signal
 
 
 def getSpectrogram(abf: pyabf.ABF, channel: int, stepLength=5, windowLength=30, rowCount=400):
@@ -77,6 +79,64 @@ def plotStevOverTime(abf: pyabf.ABF, channel: int):
     plt.axis([None, None, 0, ymax])
 
 
+def smooth(signal: np.ndarray, level: float) -> np.ndarray:
+    b, a = scipy.signal.butter(3, 1/level)
+    return scipy.signal.filtfilt(b, a, signal)
+
+
+def get_binned_counts(times: list[float], bins: np.ndarray):
+    counts = np.empty(len(bins)-1) * np.nan
+    for i in range(len(counts)):
+        bin_start = bins[i]
+        bin_end = bins[i+1]
+        bin_indexes = [x for x in times
+                       if x >= bin_start and x < bin_end]
+        counts[i] = len(bin_indexes)
+    return counts
+
+
+def get_breaths_per_minute(abf: pyabf.ABF, channel=1):
+
+    # get original signal
+    ys = abf.getAllYs(channel)
+
+    # low-pass filter
+    ys = smooth(ys, 250)
+
+    # center at zero
+    ys = ys - smooth(ys, 5_000)
+
+    # detect crossings
+    zero_crossings = np.diff((ys < 0) * 1)
+    zero_crossings = np.where(zero_crossings > 0)[0]
+    breaths = []
+    for i in range(len(zero_crossings) - 1):
+        i1 = zero_crossings[i]+1
+        i2 = zero_crossings[i+1]
+        segment = ys[i1:i2]
+        time = i1 / abf.sampleRate
+        amplitude = max(segment) - min(segment)
+        breath = (time, amplitude)
+        breaths.append(breath)
+
+    # remove small breaths
+    baseline_breath_amplitudes = [x[1] for x in breaths[:50]]
+    amplitude_threshold = np.mean(baseline_breath_amplitudes) * .05
+    breaths = [x for x in breaths if x[1] >= amplitude_threshold]
+    breath_times = [x[0]/60 for x in breaths]
+
+    # get binned breathing rate
+    max_bin = 25
+    bin_size = .5
+    bin_count = max_bin // bin_size
+    bins = np.arange(bin_count + 1) * bin_size
+    bin_edges = bins[:-1]
+    counts = get_binned_counts(breath_times, bins)
+    bpm = counts / bin_size
+
+    return (bin_edges, bpm)
+
+
 def plotSpectrogramAndPowerOverTime(abf: pyabf.ABF, ax: matplotlib.axes.Axes):
 
     # calculate spectrogram
@@ -102,7 +162,11 @@ def plotSpectrogramAndPowerOverTime(abf: pyabf.ABF, ax: matplotlib.axes.Axes):
     plotStevOverTime(abf, 0)
 
     # bottom right
+    bins, bpm = get_breaths_per_minute(abf)
     plt.subplot(224, sharex=ax2)
-    plotStevOverTime(abf, 1)
-
+    plt.plot(bins, bpm, '.-')
+    plt.ylabel("Breaths per Minute")
+    plt.xlabel("Time (minutes)")
+    plt.axis([None, None, 0, None])
+    plt.grid(alpha=.5, ls='--')
     plt.tight_layout()
